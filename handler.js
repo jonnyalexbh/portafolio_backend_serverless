0a4c9fedd123ff7c2e0e9d69a811f8e8ab0ced79
jonnyalexbh <jonnyalex.bh@gmail.com>
2021-06-03 21:24:25 -0500

    Get portfolio

--- handler.js
+++ handler.js
@@ -1,6 +1,15 @@
+const { getPortfolio } = require('./src/services/portfolios');
+
 module.exports.getPortfolio = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ data: 'Get Portfolio' }),
-  };
-};
+  const portfolio = await getPortfolio(event.queryStringParameters.id);
+
+  if (portfolio.Item) {
+    return { statusCode: 200, body: JSON.stringify(portfolio) };
+  } else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ statusCode: 404, error: `User not found` }),
+    };
+  }
+}
+
---
+++ src/services/portfolios.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk');
+const config = require('../config');
+const logger = require('../logger');
+
+const awsConfig = {
+  ...new AWS.Config({
+    endpoint: config.dynamodb.endpoint,
+    accessKeyId: config.amazon.accessKeyId,
+    secretAccessKey: config.amazon.secretAccessKey,
+    region: config.amazon.region,
+  }),
+};
+
+const db = new AWS.DynamoDB.DocumentClient(awsConfig);
+const tableName = config.dynamodb.table;
+
+exports.getPortfolio = (id) => {
+  const params = {
+    TableName: tableName,
+    Key: {
+      id,
+    },
+  };
+
+  return db
+    .get(params)
+    .promise()
+    .then((item) => item)
+    .catch((error) => {
+      logger.error(error);
+      throw new Error('An error occurred while trying to get the portfolio');
+    });
+};
